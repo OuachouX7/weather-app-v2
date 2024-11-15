bf2d666c0f68aa04df85a4d5058551c8725cc85f
OuachouX7 <ouachou9@gmail.com>
2024-11-15 21:58:21 +0100

    dropp

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -557,6 +557,7 @@ const Response = () => {
               <img className="fllag" src={ville.src} alt="" />
               <p>{ville.name}</p>
             </span>
+            
           </>
         ))}
       </div>
